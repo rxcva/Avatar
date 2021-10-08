f263857f956ac78f080ad652df1f5d07b69c80e4
rxcva <vlada.bulai20@gmail.com>
2021-10-08 13:23:20 +0300

    hi

--- src/Avatar.jsx
+++ src/Avatar.jsx
@@ -1,16 +1,16 @@
-import React  from "react";  
- 
-export function Avatar(props) {
-    return (
-      <div > 
-          
-          
-       
-      <img src={props.avatar} style={{width:'60px', height:'60px',borderRadius:'50%'}} />  
-      <h6>  {props.fullname}</h6>
-       
+import React from "react";
 
-        
-      </div>
-    );
-  }
+export function Avatar(props) {
+  return (
+    <div style={{}}>
+
+
+
+      <img src={props.avatar} style={{ width: '60px', height: '60px', borderRadius: '50%' }} />
+      <h6 style={{ textAlign: 'center' }}> {props.fullname} </h6>
+
+
+
+    </div>
+  );
+}
